15c6f9d1bc0bfaa9889ac4db83b1c52803082fd6
Stephanie KRUS <french@chezleskrus.com>
2020-11-13 21:11:28 +0000

    add views for spotify ehtics assessment

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,16 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+router.post('/spotify/spotify-physical', function (req, res) {
+    res.redirect('/spotify/spotify-emotional')
+  })
+
+router.post('/spotify/spotify-emotional', function (req, res) {
+    res.redirect('/spotify/spotify-societal')
+  })
+
+router.post('/spotify/spotify-societal', function (req, res) {
+    res.redirect('/spotify/spotify-next-steps')
+  })
